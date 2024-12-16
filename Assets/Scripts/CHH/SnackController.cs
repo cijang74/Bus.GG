59cd3e8edbfe2f26627ba411e7c094c2c3c338f6
Cho Hyeon Ho <chohh0914@naver.com>
2024-12-16 16:50:10 +0900

    좌석에 안떨어지게 설정
    Snack Controller의 Seat 배열 늘리고
    Seat만들어서 넣어주면 Transform 읽어서 그 위치엔 안 떨어짐

--- Assets/Scripts/CHH/SnackController.cs
+++ Assets/Scripts/CHH/SnackController.cs
@@ -27,6 +27,7 @@ public class SnackController : MonoBehaviour
     float minSpawnDelay = 1f;
     float maxSpawnDelay = 5f;
 
+    [SerializeField] Transform[] seat;
 
     bool[] canSpawnPoint;       //-8 ~ 8이라면 index 0번은 -8을 가리킴. 위치 - left = index
 
@@ -55,7 +56,7 @@ public class SnackController : MonoBehaviour
     {
         while(true)
         {
-            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnDelay, maxSpawnDelay));
+            yield return new WaitForSeconds(0.3f);//UnityEngine.Random.Range(minSpawnDelay, maxSpawnDelay));
             SpawnSnack();
         }
     }
@@ -63,6 +64,7 @@ public class SnackController : MonoBehaviour
     private void SpawnSnack()
     {
         ChooseSpawnSnack();
+        CheckSeat();
 
         int arrayX;
         int count = 0;
@@ -79,6 +81,27 @@ public class SnackController : MonoBehaviour
         Vector2 spawnPoint = new Vector2(arrayX * spawnGap, 0);
 
         Instantiate(toSpawnSnack, spawnPoint, Quaternion.identity);
+    }
+
+    private void CheckSeat()
+    {
+        int seatCount = seat.Length;
+        float seatLeft;
+        float seatRight;
+        int seatLeftIndex;
+        int seatRightIndex;
+        for(int i = 0; i < seatCount; i++)
+        {
+            seatLeft = seat[i].position.x - (seat[i].localScale.x / 2);
+            seatRight = seat[i].position.x + (seat[i].localScale.x / 2);
+            seatLeftIndex = Convert.ToInt32(Math.Truncate(seatLeft / spawnGap)) - leftRange;
+            seatRightIndex = Convert.ToInt32(Math.Truncate(seatRight / spawnGap)) - leftRange;
+            
+            for(int j = seatLeftIndex; j <= seatRightIndex; j++)
+            {
+                canSpawnPoint[j] = false;
+            }
+        }
     }
 
     private void ChooseSpawnSnack()
