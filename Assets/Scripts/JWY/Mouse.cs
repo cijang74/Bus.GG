63f15ef07bc9ed973da70bcaff87e901af721f91
toyede <toyede@naver.com>
2024-12-16 14:57:24 +0900

    배고픔 시스템
    .

--- Assets/Scripts/JWY/Mouse.cs
+++ Assets/Scripts/JWY/Mouse.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,11 +6,15 @@ using UnityEngine.InputSystem;
 
 public class Mouse : MonoBehaviour
 {
-    [SerializeField] float runSpeed = 10f;
+    [SerializeField] public float runSpeed = 10f;
+    [SerializeField] public float full = 100f;     //포만감, 계속해서 감소
+    [SerializeField] private float decreaseInterval = 1f;   //포만감 감소 간격(초)
+
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
-    
+    private float timeSineLastDecrease = 0f;
+
 
 
     void Start()
@@ -22,6 +26,7 @@ public class Mouse : MonoBehaviour
     void Update()
     {
         Run();
+        DecreaseFullness();
     }
 
     void OnMove(InputValue value)
@@ -34,6 +39,26 @@ public class Mouse : MonoBehaviour
         Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y);
 
         myRigidbody.velocity = playerVelocity;
+    }
+
+    private void DecreaseFullness()
+    {
+        timeSineLastDecrease += Time.deltaTime;     //프레임 시간 누적
+
+        if(timeSineLastDecrease >= decreaseInterval)
+        {
+            if (full > 0)
+            {
+                full -= 1;
+                Debug.Log("포만감 감소: " + full);
+            }
+            else
+            {
+                Debug.Log("포만감이 0입니다.");
+            }
+
+            timeSineLastDecrease = 0f;
+        }
     }
 
 }
